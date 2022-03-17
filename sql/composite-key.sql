47d5ad1a10285571afde4ddf0617c084f30f39fa
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-03-17 20:49:30 -0300

    feat: Adiciona chave primária composta e chave estrangeira

---
+++ sql/composite-key.sql
@@ -0,0 +1,52 @@
+USE [master];
+
+GO
+
+DROP DATABASE [Curso];
+USE [Curso];
+
+GO
+
+CREATE TABLE [Aluno](
+    [Id] INT NOT NULL,
+    [Nome] NVARCHAR(80) NOT NULL,
+    [Email] NVARCHAR(180) NOT NULL,
+    [Nascimento] DATETIME NULL,
+    [Ativo] BIT NOT NULL DEFAULT(0),
+
+    CONSTRAINT [PK_Aluno] PRIMARY KEY([Id]),
+    CONSTRAINT [UQ_Aluno_Email] UNIQUE([Email])
+);
+
+GO
+
+CREATE TABLE [Categoria](
+    [Id] INT NOT NULL,
+    [Nome] NVARCHAR(80) NOT NULL,
+
+    CONSTRAINT [PK_Categoria] PRIMARY KEY([Id])
+);
+
+GO
+
+CREATE TABLE [Curso](
+    [Id] INT NOT NULL,
+    [Nome] NVARCHAR(80) NOT NULL,
+    [CategoriaId] INT NOT NULL,
+
+    CONSTRAINT [PK_Curso] PRIMARY KEY([Id]),
+    CONSTRAINT [FK_Curso_Categoria] FOREIGN KEY([CategoriaId]) REFERENCES [Categoria]([Id])
+);
+
+GO
+
+CREATE TABLE [ProgressoCurso](
+    [AlunoId] INT NOT NULL,
+    [CursoId] INT NOT NULL,
+    [Progresso] INT NOT NULL,
+    [UltimaAtualizacao] DATETIME NOT NULL DEFAULT(GETDATE()),
+
+    CONSTRAINT [PK_ProgressoCurso] PRIMARY KEY([AlunoId], [CursoId]),
+    CONSTRAINT [FK_ProgressoCurso_Aluno] FOREIGN KEY([AlunoId]) REFERENCES [Aluno]([Id]),
+    CONSTRAINT [FK_ProgressoCurso_Curso] FOREIGN KEY([CursoId]) REFERENCES [Curso]([Id])
+);
