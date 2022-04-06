6cd3a8c07e521c4fbeaa82d2f9f4cc5d7201cc80
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-04-06 16:32:29 -0300

    feat: Aplica uso do `GROUP BY`

--- sql/select.sql
+++ sql/select.sql
@@ -1,14 +1,10 @@
--- UNION - Unir duas queries (as colunas precisam ser do mesmo tipo de dado)
--- UNION ALL - Executa um DISTINCT
-
 SELECT TOP 10
-    [Id],
-    [Nome]
-FROM
-    [Curso]
-UNION ALL
-SELECT TOP 10
-    [Id],
-    [Nome]
+    [Categoria].[Nome],
+    COUNT([Curso].[CategoriaId]) AS [Cursos]
 FROM
     [Categoria]
+INNER JOIN [Curso] ON
+    [Categoria].[Id] = [Curso].[Id]
+GROUP BY
+    [Categoria].[Nome],
+    [Curso].[CategoriaId] AS [Cursos]
