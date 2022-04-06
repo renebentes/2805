ca32354ea3e92b479368e105b035931bf71b9262
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-04-06 18:05:44 -0300

    feat: Aplica uso de variáveis na SP

--- sql/procedure.sql
+++ sql/procedure.sql
@@ -1,5 +1,12 @@
-CREATE OR ALTER PROCEDURE [spListCourse] AS
+CREATE OR ALTER PROCEDURE [spListCourse]
+    @Category NVARCHAR(60)
+AS
+    DECLARE @CategoryId INT
+    SET @CategoryId = (SELECT [Id] FROM [Categoria] WHERE [Nome] = @Category)
+
     SELECT
         *
     FROM
         [Curso]
+    WHERE
+        [CategoriaId] = @CategoryId
